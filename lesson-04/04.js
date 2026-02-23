78c095d5839d19db91e2a483bd3aed2c53949680
AnnaTi <anyamaslakova0@gmail.com>
2026-02-23 17:20:30 +0300

    lesson04/04

--- lesson-04/04.js
+++ lesson-04/04.js
@@ -12,4 +12,11 @@
 Пример работы функции: `doubleEachCharacter('hello')` вернёт `'hheelllloo'`
 */
 
-function doubleEachCharacter() {}
+function doubleEachCharacter(str) {
+    let newStr = '';
+for (let i = 0; i < str.length; i++){
+  let string = str[i];
+  newStr += string + string
+}
+return newStr
+}
